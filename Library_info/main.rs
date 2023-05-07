395fcdb8383dd8c02eeb988802d278a6b9485031
rabdology <67000288+rabdology@users.noreply.github.com>
2023-05-08 04:20:37 +0900

    Update main.rs

--- Library_info/main.rs
+++ Library_info/main.rs
@@ -10,13 +10,3 @@ use oauth2::{
 };
 use google_calendar3::{api::EventsInsertCall, CalendarHub};
 
-// Googleのアクセストークンを取得する定義
-async fn authenticate_google() -> AccessToken {
-    // ここに `authenticate_google` 関数の実装を追加
-}
-#[derive(Debug, Deserialize, Serialize)]
-struct Book {
-    title: String,
-    author: String,
-    due_date: DateTime<Utc>,
-}
